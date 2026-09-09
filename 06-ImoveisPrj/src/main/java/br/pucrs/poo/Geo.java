7d23afa5bbe011b9120a3ba643d08aaa544bae24
Alexandre Agustini <alexandre.agustini@pucrs.br>
2026-09-09 11:19:34 +0000

    Código da prova P1 Exemplo

---
+++ 06-ImoveisPrj/src/main/java/br/pucrs/poo/Geo.java
@@ -0,0 +1,14 @@
+package br.pucrs.poo;
+
+public class Geo {
+
+    private double latitude;
+    private double longitude;
+
+    public Geo (double lat, double longit) {
+        this.latitude = lat;
+        this.longitude = longit;
+    }
+
+
+}
---
+++ 06-ImoveisPrj/src/main/java/br/pucrs/poo/Imovel.java
@@ -0,0 +1,42 @@
+package br.pucrs.poo;
+
+public abstract class Imovel {
+    private String proprietario;
+    private int areaConstruida;
+    private Geo geoLocalizacao;
+
+    private static double VR;
+
+    public static void setVR(double valor) {
+        VR = valor;
+    }
+
+    public static double getVR() {
+        return VR;
+    }
+
+    public Imovel(String propr, int area, Geo geoLoc) {
+        if (propr == null || propr == "" )
+            throw new IllegalArgumentException("nome nao pode ser null");
+        this.proprietario = propr;
+        this.areaConstruida = area;
+        this.geoLocalizacao = geoLoc;
+    }
+
+    public int getAreaConstruida(){
+        return areaConstruida;
+    }
+
+    public String getProprietario(){
+        return proprietario;
+    }
+
+    public abstract double calculaImposto();
+
+    public String toString() {
+        return String.format("Proprietario: %s  Imposto: R$ %.2f",
+                                this.proprietario,
+                                this.calculaImposto());
+
+    }
+}
